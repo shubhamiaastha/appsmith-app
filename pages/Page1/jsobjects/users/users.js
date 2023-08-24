8853047689f1faa0f49db6465fa10bbbb4697b83
Shubham Dubey <shubhamdubey0312@gmail.com>
2023-08-24 04:52:22 +0000

    chnages

--- pages/Page1/jsobjects/users/users.js
+++ pages/Page1/jsobjects/users/users.js
@@ -5,10 +5,13 @@ export default {
 		const email = Form1.data.email ;
 		const phone = Form1.data.phone;
 		const address = Form1.data.address;
+			storeValue("fullname",fullname)
 		
 		return user.run({fullname,email,phone,address})
 	    
-		.then(()=> navigateTo('Page2'))
+		.then(()=>{ 
+			navigateTo('Page2');
+		})
 		
 	}
 	
